65e387a3025960a2fd62d5ea44d6669ba0d537cc
Elijah Bartolome <ebartolome@berkeley.edu>
2023-01-08 22:13:13 -0800

    support for showing/hiding folders

--- app/app.py
+++ app/app.py
@@ -8,36 +8,6 @@ app = Flask(__name__)
 BASE_DIR = '/usr/src/app/web_hw_files'
 
 # root webpage
-'''
-@app.route('/', defaults={'req_path': ''})
-@app.route('/<path:req_path>')
-def root(req_path):
-    # Joining the base and the requested path
-    print("Start of function")
-    abs_path = os.path.join(BASE_DIR, req_path)
-
-    # Return 404 if path doesn't exist
-    if not os.path.exists(abs_path):
-        print("Can't find path")
-        return abort(404)
-
-    # Check if path is a file and serve
-    if os.path.isfile(abs_path):
-        return downloadFile(abs_path)
-
-    # Show directory contents
-    print("abs_path: " + abs_path)
-    files = os.listdir(abs_path)
-    isFile = []
-    for file in files:
-        print(os.path.join(abs_path, file))
-        print(os.path.isfile(os.path.join(abs_path, file)))
-        isFile.append(os.path.isfile(os.path.join(abs_path, file)))
-    print("Try to render html")
-
-    return render_template('index.html', zipFiles=zip(files, isFile))
-'''
-
 @app.route('/')
 def root():
     html = directoryTree(BASE_DIR=BASE_DIR)
@@ -45,33 +15,37 @@ def root():
     return render_template('index.html', htmlTree=html)
 
 def directoryTree(BASE_DIR):
-    html = '<div id="myDIV">'
+    html = '<div id="hierarchy">'
     currLevel = 0
     for root, dirs, files in os.walk(BASE_DIR, topdown=True):
-        print(root)
+        html += '<div class="foldercontainer">' 
+
         level = root.replace(BASE_DIR, '').count(os.sep)
         if level > currLevel:
             currLevel = level
-            html += '<div id="myDIV">'
         elif level < currLevel:
             while level < currLevel:
                 html += '</div>'
                 currLevel -= 1
-        indent = '&nbsp;' * 4 * level
-        if len(files) == 0:
-            html += '<div id="myDIV">' + '{}{}'.format(indent, os.path.basename(root)) + '</div>'
+   
+        if len(files) == 0 and len(dirs) == 0:
+            html += '<span class="folder fa-folder">{}</span>'.format(os.path.basename(root))
+            html += '<span class="noitems">No Items</span>'
+            html += '</div>'
         else:
-            html += '{}{}'.format(indent, os.path.basename(root))
-            subindent = '&nbsp;' * 4 * (level + 1)
+            html += '<span class="folder fa-folder-o" data-isexpanded="true">{}</span>'.format(os.path.basename(root))
             for f in files:
-                html += '<p>' + '{}{}'.format(subindent, f) 
+                html += '<span class="file">{}'.format(f) 
                 html += '<a href="/download/{}/{}"> Download </a>'.format(root, f)
-                html += '</p>'
+                html += '</span>'
+            if len(dirs) == 0:
+                html += '</div>'
 
     if level < currLevel:
         while level < currLevel:
             html += '</div>'
             currLevel -= 1
+    html += '</div>'
     return html
 
 def previewFile():
